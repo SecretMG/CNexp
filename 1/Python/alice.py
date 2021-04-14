7f0efdf2e94de467aa9717370ba17e9af6e72bbd
ZhengZH <chnzzh@outlook.com>
2021-04-14 19:35:28 +0800

    add pdu.py
    PDU结构定义
    CRC检验

--- 1/Python/alice.py
+++ 1/Python/alice.py
@@ -3,6 +3,7 @@
 '''
 import socket
 from args import args
+from pdu import PDU, unpack_pdu, crc_check
 
 port = args.port
 data_size = args.data_size
@@ -11,7 +12,7 @@ data_size = args.data_size
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('127.0.0.1', port))
 
-
+"""
 while True:
     data, addr = s.recvfrom(data_size)  # 返回数据和接入连接的（客户端）地址
     data = data.decode()
@@ -20,4 +21,14 @@ while True:
     print('[Received]', data)
     send = input('Input: ')
     s.sendto(send.encode(), addr)  # UDP 是无状态连接，所以每次连接都需要给出目的地址
+s.close()
+"""
+
+data, addr = s.recvfrom(PDU.size)  # 返回数据和接入连接的（客户端）地址
+print(len(data))
+if crc_check(data):
+    ack, seq, info = unpack_pdu(data)
+    print(ack, seq, info)
+send_frame = PDU(0, 1, 'alicesay01')
+s.sendto(send_frame.bin_pack, addr)  # UDP 是无状态连接，所以每次连接都需要给出目的地址
 s.close()
--- 1/Python/bob.py
+++ 1/Python/bob.py
@@ -3,6 +3,7 @@
 '''
 import socket
 from args import args
+from pdu import PDU, unpack_pdu, crc_check
 
 port = args.port
 data_size = args.data_size
@@ -10,7 +11,7 @@ data_size = args.data_size
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
+"""
 while True:
     trigger = input('Input: ')
     s.sendto(trigger.encode(), ('127.0.0.1', port))
@@ -19,4 +20,14 @@ while True:
     print('[Recieved]', data)
     if trigger == '###':  # 自定义结束字符串
         break
+s.close()
+"""
+
+send_frame = PDU(1, 2, 'boblasdfs啊')
+
+s.sendto(send_frame.bin_pack, ('127.0.0.1', port))
+data, addr = s.recvfrom(PDU.size)  # 返回数据和接入连接的（服务端）地址
+if crc_check(data):
+    ack, seq, info = unpack_pdu(data)
+    print(ack, seq, info)
 s.close()
